a460ea73e9720c721668c0ca2c4fee07ccfd9ff5
giohgio <giooh.giooo15@gmail.com>
2025-10-03 19:50:04 -0300

    Update2 app.js

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(cors({
+  origin: 'https://instagram.security.suport.metas.onrender.com' // domínio real do frontend, não do servidor
+}));
+
 
 // Conectar ao MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
